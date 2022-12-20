6f0657bb0a4602dc73fe486850ec7344c0ac5389
Nina Sitaeva <sitaeva.nn@gmail.com>
2022-12-20 22:58:14 +0100

    refactor: add current dir message

--- index.js
+++ index.js
@@ -6,21 +6,28 @@ let curPath = process.cwd();
 const userArgs = process.argv.filter((el) => el.includes('username'));
 const user = !!userArgs.length ? userArgs[0].split('=')[1] : 'Guest';
 
-stdout.write(`Welcome to the File Manager, ${user}\n`);
+stdout.write(`Welcome to the File Manager, ${user}!\n`);
 console.log(`You are currently in ${curPath}`);
 
 stdin.on('data', async (input) => {
   const operation = input.toString().trim();
-  switch (operation) {
-    case '.exit': {
-      stdout.write(`\nThank you for using File Manager, ${user}, goodbye!`);
-      process.exit();
-    }
 
-    case 'up': {
-      curPath = up(curPath);
-      console.log(curPath);
-      break;
+  try {
+    switch (operation) {
+      case '.exit': {
+        stdout.write(`\nThank you for using File Manager, ${user}, goodbye!`);
+        process.exit();
+      }
+
+      case 'up': {
+        curPath = up(curPath);
+        console.log(curPath);
+        break;
+      }
     }
+  } catch (err) {
+    console.log(err);
+  } finally {
+    stdout.write(`\nYou are currently in ${curPath}\n`);
   }
 });
