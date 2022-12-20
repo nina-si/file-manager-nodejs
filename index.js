7923bcf40a51168b58cad5d0d034065d84eece0b
Nina Sitaeva <sitaeva.nn@gmail.com>
2022-12-20 22:48:16 +0100

    feat: add username args

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import up from './modules/up.js';
 import { stdin, stdout } from 'process';
 
 let curPath = process.cwd();
-const user = 'Username';
+
+const userArgs = process.argv.filter((el) => el.includes('username'));
+const user = !!userArgs.length ? userArgs[0].split('=')[1] : 'Guest';
 
 stdout.write(`Welcome to the File Manager, ${user}\n`);
 console.log(`You are currently in ${curPath}`);
