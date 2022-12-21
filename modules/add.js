e9c69885efcefd0b833477f30262cd33972901b7
Nina Sitaeva <sitaeva.nn@gmail.com>
2022-12-21 14:29:34 +0100

    refactor: default data for add function

--- modules/add.js
+++ modules/add.js
@@ -1,10 +1,9 @@
 import fsPromises from 'fs/promises';
 import * as path from 'path';
 
-const add = async (curPath, fileName) => {
+const add = async (curPath, fileName, data = '') => {
   try {
     const filePath = path.resolve(curPath, fileName);
-    const data = '';
     await fsPromises.writeFile(filePath, data);
   } catch (err) {
     throw new Error('FS operation failed');
